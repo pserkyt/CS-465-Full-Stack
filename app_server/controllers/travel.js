bfcdbd430ed6bd4fd60eacdf39355a88d005ec85
Cameron Winningham <cam@noresidue.net>
2021-10-29 03:09:58 -0400

    mvc done

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -4,7 +4,7 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-   pageTitle = process.env.npm_package_description + ' - Travel';
+   pageTitle = 'Travlr Getaways - Travel';
    res.render('travel', { title: pageTitle, trips });
 };
 
