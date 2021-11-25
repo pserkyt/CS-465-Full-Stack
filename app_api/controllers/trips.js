bc3d72c95e74f153c46c5284be8244ec7f8a56e5
Cameron Winningham <cam@noresidue.net>
2021-11-25 02:30:28 -0500

    fixed not found error

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -6,7 +6,7 @@ const tripsList = async (req, res) => {
     model
         .find({}) // empty filler for all (finds all trips)
         .exec((err, trips) => {
-            if (!trips) {
+            if (trips.length == 0) { // (!trips returns empty array: [])
                 return res
                     .status(404) // no trips found
                     .json({ "message": "trip not found" });
@@ -27,7 +27,7 @@ const tripsFindCode = async (req, res) => {
     model
         .find({ 'code': req.params.tripCode })
         .exec((err, trip) => {
-            if (!trip) {
+            if (trip.length == 0) { // (!trip returns empty array: [])
                 return res
                     .status(404) // trip not found
                     .json({ "message": "trip not found" });
